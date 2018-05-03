9bab2f464c5046f4947fea1e1568e47c8c29aa3d
dhanvi97 <dhanvi.sreeni@gmail.com>
2018-05-04 03:06:50 +0530

    Serial 2D Completed Fully, along with generated test files

--- serial2D.cpp
+++ serial2D.cpp
@@ -2,65 +2,87 @@
 #include <vector>
 #include "Eigen/Dense"
 #include <cmath>
-#define N 8
-#define K1 1000
+#include <fstream>
+
+#define N 20
+#define K1 200
 #define K2 1
 #define OMEGA 10
+#define NUM_OF_ITER 50
 
 using namespace Eigen;
 using namespace std;
 
-MatrixXf Create_Stiffness_Matrix() {
-	MatrixXf A(3,3);
-	A << 1, 0, 0, 
-		 0, 1, 0, 
-		 0, 0, 1;
-	return A;
-}
+MatrixXf Create_Stiffness_Matrix(int n, int size, float k1, float k2) {
 
-int main(){
-	int size = N*N, count_expl = 0, count_impl = 0;
-	MatrixXf M(size,size), K(size,size), K_stiff(N,N), K_fluid(N,N), M_stiff(N,N), M_fluid(N,N);
-	MatrixXf Z = Create_Stiffness_Matrix();
+	MatrixXf K(size,size), K_fluid(n,n), K_stiff(n,n);
+	K_fluid.setZero(); K_stiff.setZero();
 
-	K.setZero(); M.setZero(); K_stiff.setZero(); K_fluid.setZero();
-
-	VectorXf K_upper_diag_stiff(N-1), K_diag_stiff(N);
-	K_upper_diag_stiff.setConstant(-1*K1);
-	K_diag_stiff.setConstant(2*K1);
+	VectorXf K_upper_diag_stiff(n-1), K_diag_stiff(n);
+	K_upper_diag_stiff.setConstant(-1*k1);
+	K_diag_stiff.setConstant(2*k1);
 	K_stiff.diagonal() = K_diag_stiff;
 	K_stiff.diagonal(1) = K_upper_diag_stiff;
 	K_stiff.diagonal(-1) = K_upper_diag_stiff;
 
-	VectorXf K_upper_diag_fluid(N-1), K_diag_fluid(N);
-	K_upper_diag_fluid.setConstant(-1*K1);
-	K_diag_fluid.setConstant(2*K1);
-	K_diag_fluid.segment(3,N-6).setConstant(2*K2);
-	K_upper_diag_fluid.segment(2,N-5).setConstant(-1*K2);
-	K_diag_fluid(2) = K1 + K2;
-	K_diag_fluid(N-3) = K1 + K2;
+	VectorXf K_upper_diag_fluid(n-1), K_diag_fluid(n);
+	K_upper_diag_fluid.setConstant(-1*k1);
+	K_diag_fluid.setConstant(2*k1);
+	K_diag_fluid.segment(3,n-6).setConstant(2*k2);
+	K_upper_diag_fluid.segment(2,n-5).setConstant(-1*k2);
+	K_diag_fluid(2) = k1 + k2;
+	K_diag_fluid(N-3) = k1 + k2;
 	K_fluid.diagonal() = K_diag_fluid;
 	K_fluid.diagonal(-1) = K_upper_diag_fluid;
 	K_fluid.diagonal(1) = K_upper_diag_fluid;
 
-	VectorXf M_diag_stiff(N);
+	for(int i = 0; i < n; i++){
+		if(i < 3 || i > N-4)
+			K.block(i*n,i*n,n,n) = K_stiff;
+		else
+			K.block(i*n,i*n,n,n) = K_fluid;
+	}
+
+	return K;	
+}
+
+MatrixXf Create_Mass_Matrix(int n, int size) {
+
+	MatrixXf M(size,size), M_fluid(n,n), M_stiff(n,n);
+	M_fluid.setZero(); M_stiff.setZero();	
+
+	VectorXf M_diag_stiff(n);
 	M_diag_stiff.setConstant(1);
 	M_stiff.diagonal() = M_diag_stiff;
 
-	VectorXf M_diag_fluid(N);
+	VectorXf M_diag_fluid(n);
 	M_diag_fluid.setConstant(1);
 	M_fluid.diagonal() = M_diag_fluid;
 
-	for(int i = 0; i < N; i++){
-		if(i < 3 || i > N-4){
-			K.block(i*N,i*N,N,N) = K_stiff;
-			M.block(i*N,i*N,N,N) = M_stiff;
-		}
-		else{
-			K.block(i*N,i*N,N,N) = K_fluid;
-			M.block(i*N,i*N,N,N) = M_fluid;
-		}
+	for(int i = 0; i < n; i++){
+		if(i < 3 || i > n-4)
+			M.block(i*n,i*n,n,n) = M_stiff;
+		else
+			M.block(i*n,i*n,n,n) = M_fluid;
 	}
+
+	return M;	
+}
+
+int main(){
+	ofstream output_x, output_y;
+	output_x.open("u_x.txt");
+	output_y.open("u_y.txt");
+	int size = N*N, count_expl = 0, count_impl = 0;
+	float delta_t = 1;
+
+	MatrixXf M(size,size), K(size,size);
+
+	K.setZero(); M.setZero();
+
+	K = Create_Stiffness_Matrix(N, size, K1, K2);
+	M = Create_Mass_Matrix(N, size);
+
 
 	vector<int> ordering;
 	int permutation[size];
@@ -111,8 +133,6 @@ int main(){
 			 u_old_i_y(count_impl), v_old_i_y(count_impl), a_old_i_y(count_impl),
 			 u_new_i_y(count_impl), v_new_i_y(count_impl), a_new_i_y(count_impl), 
 			 Fi_y(count_impl), Fe_y(count_expl);
-	
-	float delta_t = 1;
 
 	u_old_e_x.setZero();
 	v_old_e_x.setZero();
@@ -121,15 +141,24 @@ int main(){
 	v_old_i_x.setZero();
 	a_old_i_x.setZero();
 
+	u_old_e_y.setZero();
+	v_old_e_y.setZero();
+	a_old_e_y.setZero();
+	u_old_i_y.setZero();
+	v_old_i_y.setZero();
+	a_old_i_y.setZero();
+
 	Fi_x.setZero();
 	Fe_x.setZero();
 		
 	A = M_i + K_i*(delta_t*delta_t/4);
 	B = A.inverse();
 
+	MatrixXf u_x(N,N), u_y(N,N);
+	MatrixXf buffer = u_new_e_x;
+	u_x.setZero(); u_y.setZero();
 
-
-	for (int i = 0; i < 10000; i++) {
+	for (int i = 0; i < NUM_OF_ITER; i++) {
 		a_new_e_x = (Fe_x - K_e*u_old_e_x - K_ei*u_old_i_x);
 		v_new_e_x = v_old_e_x + (delta_t/2) * ( a_old_e_x + a_new_e_x);
 		u_new_e_x = u_old_e_x + delta_t*v_new_e_x + (delta_t*delta_t/2)*a_new_e_x;
@@ -148,7 +177,7 @@ int main(){
 		
 		Fe_x.setConstant(OMEGA*i*delta_t);
 		Fe_x = cos(Fe_x.array());
-		Fe_x = 10*Fe_x;
+		Fe_x = 0.5*Fe_x;
 
 		a_new_e_y = (Fe_y - K_e*u_old_e_y - K_ei*u_old_i_y);
 		v_new_e_y = v_old_e_y + (delta_t/2) * ( a_old_e_y + a_new_e_y);
@@ -168,10 +197,29 @@ int main(){
 		
 		Fe_y.setConstant(OMEGA*i*delta_t);
 		Fe_y = sin(Fe_y.array());
-		Fe_y = 10*Fe_y;
-		
-		
+		Fe_y = 0.5*Fe_y;
+
+		for(int i=0; i<N; i++) {
+			if(i<3){
+				u_x.block(i,0,1,N) = u_new_i_x.segment(i*N,N).transpose();
+				u_y.block(0,i,N,1) = u_new_i_y.segment(i*N,N);
+			}else if(i < N-3) {
+				u_x.block(i,0,1,3) = u_new_i_x.segment(3*N + (i-3)*6,3).transpose();
+				u_x.block(i,N-3,1,3) = u_new_i_x.segment(3*N +(i-3)*6+3,3).transpose();
+				u_y.block(0,i,3,1) = u_new_i_y.segment(3*N + (i-3)*6, 3);
+				u_y.block(N-3,i,3,1) = u_new_i_y.segment(3*N +(i-3)*6+3,3);
+			}else{
+				u_x.block(i,0,1,N) = u_new_i_x.segment(3*N + (N-6)*6 + (i-N+3)*N, N).transpose();
+				u_y.block(0,i,N,1) = u_new_i_y.segment(3*N + (N-6)*6 + (i-N+3)*N, N);
+			}
+		}
+		buffer.resize(N-6,N-6);
+		u_x.block(3,3,N-6,N-6) = buffer;
+		buffer = u_new_e_y;
+		buffer.resize(N-6,N-6);
+		u_y.block(3,3,N-6,N-6) = buffer.transpose();
+
+		output_x << u_x <<'\n';
+		output_y << u_y <<'\n';
 	}
-	//cout << u_new_i_x << endl;
-	cout << u_new_e_x << endl;
 }
